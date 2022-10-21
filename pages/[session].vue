9ec697fd98519cbd33884896ffd375eef81ef915
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-10-21 16:24:20 +0300

    ف

--- pages/[session].vue
+++ pages/[session].vue
@@ -1,25 +1,28 @@
 <template>
     <div h="full" flex="~">
         <!-- left -->
-        <div flex="~ basis-1/2">
-            <iframe h="full" w="full" ref="previewRef"></iframe>
+        <div flex="~ col basis-1/2">
+            <iframe border="0" id="preview" flex="grow" w="full" ref="previewRef"></iframe>
+            <div h="200px" bg="secondary dark:secondaryOp">
+                {{ logMessages }}
+            </div>
         </div>
 
 
         <!-- right -->
-        <div flex="~ col gap-4 basis-1/2" border="l secondary dark:secondaryOp">
+        <div flex="~ col gap-4 basis-1/2" border="l-2 secondary dark:secondaryOp">
             <!-- Tabs -->
             <UiTabGroup h="full" :tabs="['html', 'css', 'js']">
                 <template #tab-1>
-                    <MonacoEditor w="full" h="full" v-model="code.html" lang="html" :options="{ theme: 'vs-dark'}" />
+                    <MonacoEditor w="full" h="full" v-model="code.html" lang="html" :options="{ theme: 'vs-dark', fontSize: '20px' }" />
                 </template>
 
                 <template #tab-2>
-                    <MonacoEditor w="full" h="full" v-model="code.css" lang="css" :options="{ theme: 'vs-dark'}" />
+                    <MonacoEditor w="full" h="full" v-model="code.css" lang="css" :options="{ theme: 'vs-dark', fontSize: '20px'}" />
                 </template>
 
                 <template #tab-3>
-                    <MonacoEditor w="full" h="full" v-model="code.javascript" lang="javascript" :options="{ theme: 'vs-dark'}" />
+                    <MonacoEditor w="full" h="full" v-model="code.javascript" lang="javascript" :options="{ theme: 'vs-dark', fontSize: '20px'}" />
                 </template>
             </UiTabGroup>
 
@@ -57,7 +60,7 @@ const updatePreview = useDebounceFn(() => {
     const { html, css, javascript } = code.value
 
     // Get element of iframe
-    var preview = previewRef.value.contentDocument || previewRef.value.contentWindow.document;
+    const preview = previewRef.value.contentDocument || previewRef.value.contentWindow.document;
 
     preview.open();
     preview.write(`<style scoped>${css ?? ''}</style>${html ?? ''}`);
@@ -67,14 +70,53 @@ const updatePreview = useDebounceFn(() => {
     scriptEl.append(newContent)
     preview.body.appendChild(scriptEl);
     preview.close();
+
+    
+
+
+    // const c = previewRef.value?.contentWindow?.console
+    // console.log(c.log())
+    // if (c) {
+    //     c.log = (...args) => {
+    //         frameConsole.value = args
+    //     }
+    // }
 }, 100)
+
+
+var logBackup = console.log;
+var logMessages = ref([]);
+
 
 // Update preview on mounted
 onMounted( () => {
+    
+
+    
+
+    // console.log(previewRef.value.contentWindow.console.log)
+    // previewRef.value?.contentWindow.console.log = function(val) {
+    //     frameConsole.value.appendChild(document.createTextNode(val));
+    // }
+
+    // previewRef.value.contentWindow.console.log = function() {
+    //     logMessages.value.push.apply(logMessages.value, arguments);
+    //     logBackup.apply(console, arguments);
+    // };
+
+   
+
+    // window.addEventListener('message', function(event){
+    //     console.log(event.data)
+    //             //REM: Just appending it to the body.. lazy
+    //     // document.body.appendChild(document.createTextNode(event.data))
+    // }, false);
     updatePreview()
 })
 
+
+
+
 // Listen for writes in editors
 watch(() => code.value, () => updatePreview(), { deep: true })
-
 </script>
