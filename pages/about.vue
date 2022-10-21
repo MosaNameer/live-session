d6ca71aa5ec4dac1b4b6da2cf8712474115db09c
Zain A. Habeeb <13039754+eq0@users.noreply.github.com>
2022-10-21 16:42:07 +0300

    blabla

---
+++ pages/about.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        
+    asdasdasd
+    <temp>
+    <template #rightpanel>
+        <h1>sadasdasddddddddddddddddd</h1>
+    </template>
+    </temp>
+
+    </div>
+</template>
+
+<script setup>
+    definePageMeta({  layout: "TwoSections",});
+</script>
