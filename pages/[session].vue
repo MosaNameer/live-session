27e5f951a7ebdb76cf818f9a8a77ac23eab24c9d
Hasan Alkarnosy <gruceing@gmail.com>
2022-10-23 09:35:16 +0300

    fixes

--- pages/[session].vue
+++ pages/[session].vue
@@ -7,6 +7,9 @@
             <span>
                 Read Only: {{ sessionData?.readOnly }} <UiButton @click="toggleSessionState" w="fit">Toggle State</UiButton>
             </span>
+            <p>
+                {{code.html}}
+            </p>
         </div>
 
 
@@ -15,7 +18,7 @@
             <!-- Tabs -->
             <UiTabGroup flex="basis-1/2" :tabs="['html', 'css', 'js']">
                 <template #tab-1>
-                    <MonacoEditor w="full" h="full" v-model="code.html" lang="html" :options="{ theme: 'vs-dark', fontSize: '20px', readOnly: !isAdmin && sessionData?.readOnly }" />
+                    <MonacoEditor w="full" h="full" @keyup="sendCode()" v-model="code.html" lang="html" :options="{ theme: 'vs-dark', fontSize: '20px', readOnly: !isAdmin && sessionData?.readOnly }" />
                 </template>
 
                 <template #tab-2>
@@ -66,12 +69,14 @@ const languages = [
 ]
 
 // Update preview
-const updatePreview = async () => {
+const updatePreview = useDebounceFn(async () => {
     const { html, css, javascript } = code
 
     // Get element of iframe
     const preview = previewRef.value.contentDocument || previewRef.value.contentWindow.document;
 
+    if (!preview) return false
+    
     preview.open();
     preview.write(`<style scoped>${css ?? ''}</style>${html ?? ''}`);
 
@@ -81,18 +86,17 @@ const updatePreview = async () => {
     preview.body.appendChild(scriptEl);
     preview.close();
 
-}
+}, 250)
 
 
-const sendCode = async () => {
+const sendCode = useDebounceFn(async () => {
     await $fetch('/api/code', {
         method: 'POST',
         body: JSON.stringify({
             message: code,
         })
     })
-}
-
+}, 250)
 
 
 watch(() => JSON.parse(socket.value.data), async (data) => {
@@ -115,22 +119,25 @@ watch(() => JSON.parse(socket.value.data), async (data) => {
 // Update preview on mounted
     // Send to the web socket server 
     // $socket.send("test")
-    try {
-        sessionData.value = await $fetch(`/api/session/${session}`)
-    } catch (e) {
-        router.push('/')
-    }
+try {
+    sessionData.value = await $fetch(`/api/session/${session}`)
+} catch (e) {
+    router.push('/')
+}
+
+onMounted(() => {
     if (sessionData.value?.data.type == "code"){
         code.html = sessionData.value?.data.data.html
         code.css = sessionData.value?.data.data.css
         code.javascript = sessionData.value?.data.data.javascript
         updatePreview()
     }
+})
 
 
 
 // Listen for writes in editors
-watch(() => code, () => { updatePreview(), sendCode() }, { deep: true })
+watchDebounced(() => code, () => updatePreview(), { deep: true, debounce: 250, maxWait: 1000 })
 
 
 
