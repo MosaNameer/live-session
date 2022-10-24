60a8e7e679d0cca998a6b1514fd8cb5d664836da
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-10-24 15:20:52 +0300

    suspense to wait loading.

--- pages/[session].vue
+++ pages/[session].vue
@@ -1,73 +1,79 @@
 <template>
-    <NuxtLayout name="two-sections">
-        <div flex="~ col" w="full" p="4" position="relative">
-            <span text="lg tertiaryOp dark:tertiary">{{ store.getCurrentSlide.chapter }}</span>
-            <span text="xl primaryOp dark:primary" mb="6">{{ store.getCurrentSlide.title }}</span>
+    <Suspense>
+        <NuxtLayout name="two-sections">
+            <div flex="~ col" w="full" p="4" position="relative">
+                <span text="lg tertiaryOp dark:tertiary">{{ store.getCurrentSlide.chapter }}</span>
+                <span text="xl primaryOp dark:primary" mb="6">{{ store.getCurrentSlide.title }}</span>
 
-            <!-- CONTENT -->
-            <div v-show="selectedTab === 'settings'">
-                Settings
-            </div>
+                <!-- CONTENT -->
+                <div v-show="selectedTab === 'settings'">
+                    Settings
+                </div>
 
-            <div v-show="selectedTab === 'users'">
-                <Users />
-            </div>
+                <div v-show="selectedTab === 'users'">
+                    <Users />
+                </div>
 
-            <div v-show="selectedTab == null">
-                <ContentRenderer :value="store.getSlideContent">
-                    <template #empty>
-                        <p>No content found.</p>
-                    </template>
-                </ContentRenderer>
-            </div>
+                <div v-show="selectedTab == null">
+                    <ContentRenderer :value="store.getSlideContent">
+                        <template #empty>
+                            <p>No content found.</p>
+                        </template>
+                    </ContentRenderer>
+                </div>
 
-            <br /><br />
+                <br /><br />
 
-            <div flex="~ col" position="absolute" left="0" bottom="0" w="full">
-                <div flex="~" justify="between" bg="secondary dark:secondaryOp" p="y-2 x-4">
-                    <span>{{ store.getSession?.id }}</span>
-                    <div flex="~ gap-2">
-                        <span>{{ store.getUsers?.length }}</span>
-                        <IconUsers w="24px" />
+                <div flex="~ col" position="absolute" left="0" bottom="0" w="full">
+                    <div flex="~" justify="between" bg="secondary dark:secondaryOp" p="y-2 x-4">
+                        <span>{{ store.getSession?.id }}</span>
+                        <div flex="~ gap-2">
+                            <span>{{ store.getUsers?.length }}</span>
+                            <IconUsers w="24px" />
+                        </div>
+                    </div>
+                    <div v-if="store.isAdmin" flex="~" justify="between" p="4">
+                        <div flex="~ gap-3">
+                            <span cursor="pointer">Full Screen</span>
+                            <span @click="selectTab('settings')" cursor="pointer">Settings</span>
+                            <span @click="selectTab('users')" cursor="pointer">Users</span>
+                        </div>
+                        <div flex="~ gap-6">
+                            <span :class="{ 'text-error': store.hasNextSlide }" @click="store.nextSlide()" cursor="pointer">Left</span>
+                            <span :class="{ 'text-error': store.hasPrevSlide }" @click="store.prevSlide()" cursor="pointer">Right</span>
+                        </div>
                     </div>
                 </div>
-                <div v-if="store.isAdmin" flex="~" justify="between" p="4">
-                    <div flex="~ gap-3">
-                        <span cursor="pointer">Full Screen</span>
-                        <span @click="selectTab('settings')" cursor="pointer">Settings</span>
-                        <span @click="selectTab('users')" cursor="pointer">Users</span>
-                    </div>
-                    <div flex="~ gap-6">
-                        <span :class="{ 'text-error' : store.hasNextSlide }" @click="store.nextSlide()" cursor="pointer">Left</span>
-                        <span :class="{ 'text-error' : store.hasPrevSlide }" @click="store.prevSlide()" cursor="pointer">Right</span>
-                    </div>
-                </div>
+
+
+
+
             </div>
 
 
 
-            
-        </div>
+            <!-- Right Panel -->
+            <template #rightpanel>
+                <div flex="~ col" position="relative" w="full" h="full">
+                    <div position="absolute" right="0" flex="~" bg="secondary dark:secondaryOp">
+                        <div v-if="store.isAdmin" @click="store.toggleReadOnly" cursor="pointer" border="~" p="2">{{ store.isReadOnly ? "READ ONLY" : "READ WRITE" }}</div>
+                        <div v-if="store.isAdmin" @click="store.toggleProdcast" cursor="pointer" border="~" p="2">{{ store.isProdcast ? "PRODCASTING" : "NOT PRODCASTING" }}</div>
+                        <div v-if="store.getCurrentSlide?.type == 'CodeEditor' && store.getProdcastedCode && !store.isProdcast" @click="store.setCode(store.getSession?.prodcastedData)" cursor="pointer" border="~" p="2">Restore Prodcasted Code</div>
+                    </div>
 
+                    <!-- SLIDE TYPE -->
+                    <div flex="grow">
+                        <TypeCodeEditor v-if="store.getCurrentSlide.type == 'CodeEditor'" />
+                    </div>
 
-
-        <!-- Right Panel -->
-        <template #rightpanel>
-            <div flex="~ col" position="relative" w="full" h="full">
-                <div position="absolute" right="0" flex="~" bg="secondary dark:secondaryOp">
-                    <div v-if="store.isAdmin" @click="store.toggleReadOnly" cursor="pointer" border="~" p="2">{{ store.isReadOnly ? "READ ONLY" : "READ WRITE" }}</div>
-                    <div v-if="store.isAdmin" @click="store.toggleProdcast" cursor="pointer" border="~" p="2">{{ store.isProdcast ? "PRODCASTING" : "NOT PRODCASTING" }}</div>
-                    <div v-if="store.getCurrentSlide?.type == 'CodeEditor' && store.getProdcastedCode && !store.isProdcast" @click="store.setCode(store.getSession?.prodcastedData)" cursor="pointer" border="~" p="2">Restore Prodcasted Code</div>
                 </div>
+            </template>
+        </NuxtLayout>
 
-                <!-- SLIDE TYPE -->
-                <div flex="grow">
-                    <TypeCodeEditor v-if="store.getCurrentSlide.type == 'CodeEditor'" />
-                </div>
-                
-            </div>
+        <template #fallback>
+            Loading...
         </template>
-    </NuxtLayout>
+    </Suspense>
 </template>
 
 <script setup>
