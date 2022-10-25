289eb4b1ccf88e00ec16ab18a10fafb6afe49ff1
Hassan Alkhalidi <5357529+Gruce@users.noreply.github.com>
2022-10-26 00:51:18 +0300

    ضض

--- components/type/Question.vue
+++ components/type/Question.vue
@@ -1,10 +1,22 @@
 <template>
-    <div h="full" bg="primary">
-        {{store?.getQuestions}}
+    <div flex="~ col" h="full" bg="primary">
+        <h1 text="white">{{selectedQuestion+1}} / {{questions?.length}}</h1>
+        <div flex="~ gap-2">
+            <span 
+            v-for="(question, index) in questions" :key="question.question"
+            @click="selectedQuestion = index"
+            :class="[
+                selectedQuestion == index
+                ? 'bg-white'
+                : 'bg-whitesec'
+            ]" flex="~" items="center" justify="center" w="10" h="10" text="xl" cursor="pointer">{{ index }}</span>
+        </div>
     </div>
 </template>
 
 <script setup>
 const store = useSessionStore()
+const questions = store?.getQuestions
 
+const selectedQuestion = ref(0)
 </script>
